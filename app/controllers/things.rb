4d4524354c5c0ed0e1b6a46c93f7b967665cceac
Mitch Mele <mitchmele@gmail.com>
2017-11-14 13:19:47 -0600

    Update things.rb

--- app/controllers/things.rb
+++ app/controllers/things.rb
@@ -1,41 +1,38 @@
 # RESTFUL Routing:
-# display all things
+#index
 get '/things' do
   erb :"/things/index"
 end
-
-# render a new thing form
+#create
 get '/things/new' do
   authenticate!
   erb :"/things/new"
 end
 
-# create a new thing
 post '/things' do
   authenticate!
   redirect '/things'
 end
 
-# display a specific thing
+#show
 get '/things/:id' do
   erb :"/things/show"
 end
 
-# render an edit form for a thing
+# edit
 get '/things/:id/edit' do
   authenticate!
   authorized!
   erb :"/things/:id/edit"
 end
 
-# update a thing
 put '/things/:id' do
   authenticate!
   authorized!
   redirect "/things/#{params[:id]}"
 end
 
-# delete a specific thing
+# delete 
 delete '/things/:id' do
   authenticate!
   authorized!
